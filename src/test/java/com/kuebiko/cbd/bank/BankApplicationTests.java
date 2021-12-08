13ad2eb9a947dab3e56c325b473032d81659d9f1
Sujan shrestha <ssuja@MacBook-Pro.local>
2021-12-07 23:10:42 -0600

    Added the models

--- src/test/java/com/kuebiko/cbd/bank/BankApplicationTests.java
+++ src/test/java/com/kuebiko/cbd/bank/BankApplicationTests.java
@@ -4,7 +4,6 @@ import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest(classes=BankApplicationTests.class)
 class BankApplicationTests {
-
 	@Test
 	void contextLoads() {
 	}
