a07e9bdb1a2efc0a3fb701fd4d85a8d7a7572669
Anish Thakur <real.anish9@gmail.com>
2021-12-15 12:00:00 -0600

    Transaction Validator updated

--- src/main/java/com/kuebiko/cbd/bank/model/Transaction.java
+++ src/main/java/com/kuebiko/cbd/bank/model/Transaction.java
@@ -1,7 +1,10 @@
 package com.kuebiko.cbd.bank.model;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
 import java.util.Date;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class Transaction {
     private Long invoice;
     private Date date;
