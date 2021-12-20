cc88fe7f5f315078c9f75e20d2498201941e9c24
maheshpoudyal <mahesh.poudyal@mavs.uta.edu>
2021-12-20 13:41:48 -0600

    Kafka Producer created

---
+++ src/main/java/com/kuebiko/cbd/bank/producer/KafkaProducer.java
@@ -0,0 +1,36 @@
+package com.kuebiko.cbd.bank.producer;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.stereotype.Component;
+import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.util.concurrent.ListenableFutureCallback;
+
+@Component
+public class KafkaProducer {
+
+    @Autowired
+    private KafkaTemplate kafkaTemplate;
+
+    public void send(String topic, String message){
+       // kafkaTemplate.send(topic,message);
+        ListenableFuture<SendResult<String, String>> future =
+                kafkaTemplate.send(topic, message);
+
+        future.addCallback(new ListenableFutureCallback<SendResult<String, String>>() {
+
+            @Override
+            public void onSuccess(SendResult<String, String> result) {
+                System.out.println("Sent message=[" + message +
+                        "] with offset=[" + result.getRecordMetadata().offset() + "]");
+            }
+            @Override
+            public void onFailure(Throwable ex) {
+                System.out.println("Unable to send message=["
+                        + message + "] due to : " + ex.getMessage());
+            }
+        });
+
+    }
+}
--- src/main/java/com/kuebiko/cbd/bank/service/PersonalDetailService.java
+++ src/main/java/com/kuebiko/cbd/bank/service/PersonalDetailService.java
@@ -3,18 +3,26 @@ package com.kuebiko.cbd.bank.service;
 import com.kuebiko.cbd.bank.dao.Dao;
 import com.kuebiko.cbd.bank.dao.UserDao;
 import com.kuebiko.cbd.bank.model.PersonalDetails;
+import com.kuebiko.cbd.bank.producer.KafkaProducer;
 import com.kuebiko.cbd.bank.util.PersonalDetailsValidator;
 import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PersonalDetailService extends BaseService<PersonalDetails,PersonalDetails> {
 
+
+    @Value(value = "${kafka.topic.details}")
+    private String topic;
+
     @Autowired
     PersonalDetailService(UserDao userDao){
         super(userDao);
     }
+    @Autowired
+    KafkaProducer kafkaProducer;
 
 
     @Override
@@ -26,7 +34,9 @@ public class PersonalDetailService extends BaseService<PersonalDetails,PersonalD
     @Override
     public PersonalDetails save(PersonalDetails data) throws Exception {
         PersonalDetailsValidator.validate(data);
-        return (PersonalDetails) dao.save(data);
+        PersonalDetails personalDetails = (PersonalDetails) dao.save(data);
+        kafkaProducer.send(topic, "New User has been added..");
+        return personalDetails;
     }
 
     @Override
