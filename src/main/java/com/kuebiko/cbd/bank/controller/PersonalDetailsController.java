0c5ef8e92aaf516e38fd5521b216c655230298a4
SajanaB <92946666+SajanaB@users.noreply.github.com>
2021-12-21 12:12:40 -0600

    Update PersonalDetailsController

--- src/main/java/com/kuebiko/cbd/bank/controller/PersonalDetailsController.java
+++ src/main/java/com/kuebiko/cbd/bank/controller/PersonalDetailsController.java
@@ -4,6 +4,8 @@ import com.kuebiko.cbd.bank.model.Payment;
 import com.kuebiko.cbd.bank.model.PersonalDetails;
 import com.kuebiko.cbd.bank.service.BaseService;
 import com.kuebiko.cbd.bank.service.PersonalDetailService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +14,8 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/details")
 public class PersonalDetailsController extends BaseController {
+
+    Logger logger = LoggerFactory.getLogger(PersonalDetailsController.class);
 
     @Autowired
     public PersonalDetailsController(PersonalDetailService personalDetailService) {
@@ -22,10 +26,13 @@ public class PersonalDetailsController extends BaseController {
     @RequestMapping("/save")
     public ResponseEntity<PersonalDetails> saveOrder(@RequestBody PersonalDetails payload)
     {
+        logger.info("new user add request");
         PersonalDetails response = new PersonalDetails();
         try {
                 response =  (PersonalDetails) getService().save(payload);
+                logger.info("successfully saved");
         } catch (Exception e) {
+            logger.error(e.getMessage());
             response.setMessage(e.getMessage());
             return ResponseEntity.badRequest().body(response);
         }
