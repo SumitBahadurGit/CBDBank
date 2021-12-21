5df31464a29f10a7e7a576d2225032c8e8719a23
ShimonBhandari <72523905+ShimonBhandari@users.noreply.github.com>
2021-12-20 22:28:30 -0600

    Add files via upload

---
+++ src/main/java/com/kuebiko/cbd/bank/dao/OrderDao.java
@@ -0,0 +1,41 @@
+package com.kuebiko.cbd.bank.dao;
+
+import com.kuebiko.cbd.bank.entity.OrderEntity;
+import com.kuebiko.cbd.bank.mapper.OrderMapper;
+import com.kuebiko.cbd.bank.model.Order;
+import com.kuebiko.cbd.bank.repo.OrderRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.persistence.Column;
+
+@Component
+public class OrderDao extends Dao<Order>{
+
+    @Autowired
+    private OrderRepo orderRepo;
+
+    @Override
+    public Order save(Order order) {
+
+        OrderEntity orderEntity = OrderMapper.map(order);
+        orderEntity = orderRepo.save(orderEntity);
+
+        Order returnOrder = OrderMapper.map(orderEntity);
+        return returnOrder;
+    }
+
+    @Override
+    public Order find(Long id) {
+        OrderEntity orderEntity = orderRepo.getById(id);
+
+        Order returnOrder = OrderMapper.map(orderEntity);
+
+        return returnOrder;
+    }
+
+    @Override
+    public Order find(Long id, Long userId) {
+        return null;
+    }
+}
